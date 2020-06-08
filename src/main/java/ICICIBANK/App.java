2aba501f3886ba5bf78820340f3c5b7f75d76e1e
sudhakar <trainer@gmail.com>
2020-06-08 07:53:54 +0530

    new nexus project added

---
+++ src/main/java/ICICIBANK/App.java
@@ -0,0 +1,13 @@
+package ICICIBANK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
